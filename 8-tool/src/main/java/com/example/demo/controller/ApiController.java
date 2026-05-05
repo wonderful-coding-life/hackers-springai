f6f3dd6ea8a1fee2441b46ed93e3b7271502e790
spiderman <spiderman@xhift.co.kr>
2026-05-05 09:03:57 +0900

    added tool projects

---
+++ 8-tool/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.tool.ProductOrderTool;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class ApiController {
+    @Autowired
+    private OpenAiChatModel chatModel;
+    @Autowired
+    private ProductOrderTool productOrderTool;
+
+    private static final String systemMessage = """
+            당신은 해커스 쇼핑몰의 고객지원 상담사야.
+            답변은 짥고 명료하게 해 줘.
+            """;
+
+    @PostMapping("/chats")
+    public String postChats(@RequestParam("message") String userMessage) {
+        List<Message> messages = List.of(
+                new UserMessage(userMessage),
+                new SystemMessage(systemMessage)
+        );
+        ChatOptions chatOptions = OpenAiChatOptions.builder()
+                .toolCallbacks(ToolCallbacks.from(productOrderTool))
+                .build();
+        Prompt prompt = new Prompt(messages, chatOptions);
+        ChatResponse response = chatModel.call(prompt);
+        return response.getResult().getOutput().getText();
+    }
+}
