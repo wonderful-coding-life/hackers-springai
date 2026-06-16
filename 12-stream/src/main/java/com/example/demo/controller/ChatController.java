942a1e06f9fee8508f0e2f589ebdf40ea025bf96
spiderman <spiderman@xhift.co.kr>
2026-06-16 22:07:37 +0900

    updated 12 with Spring AI 2.0, gradle 9.5.1

--- 12-stream/src/main/java/com/example/demo/controller/ChatController.java
+++ 12-stream/src/main/java/com/example/demo/controller/ChatController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +19,8 @@ public class ChatController {
     private final OpenAiChatModel chatModel;
     private final ObjectMapper objectMapper;
 
-    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> getChatResponse(@RequestParam("message") String message) {
+    //@GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getChatResponseChatModel(@RequestParam("message") String message) {
 
         //return chatModel.stream(message);
 
@@ -27,6 +29,32 @@ public class ChatController {
         // ObjectMapper.writeValueAsString()은 Java 문자열을 JSON 문자열 형태로 안전하게 직렬화하며,
         // 따옴표 추가와 escape 처리까지 자동 수행한다.
         Flux<String> stream = chatModel.stream(message)
+                .map(objectMapper::writeValueAsString)
+                .doOnSubscribe(s -> log.info("3. subscribed"))
+                .doOnNext(token -> log.info("4. token: {}", token))
+                .doOnComplete(() -> log.info("5. stream complete"));
+
+        log.info("2. controller return");
+
+        return stream;
+    }
+
+    @Autowired
+    private ChatClient chatClient;
+
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getChatResponse(@RequestParam("message") String message) {
+
+        // return chatClient.prompt().user(message).stream().content().map(objectMapper::writeValueAsString);
+
+        log.info("1. request start");
+
+        // ObjectMapper.writeValueAsString()은 Java 문자열을 JSON 문자열 형태로 안전하게 직렬화하며,
+        // 따옴표 추가와 escape 처리까지 자동 수행한다.
+        Flux<String> stream = chatClient.prompt()
+                .user(message)
+                .stream()
+                .content()
                 .map(objectMapper::writeValueAsString)
                 .doOnSubscribe(s -> log.info("3. subscribed"))
                 .doOnNext(token -> log.info("4. token: {}", token))
